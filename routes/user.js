1da75fa77a85f768f4d1291314179569b4722b4b
Himanshi sharma <sharmahimanshi877@gmail.com>
2025-08-11 15:27:23 +0530

    Initial commit

---
+++ routes/user.js
@@ -0,0 +1,13 @@
+// const express=require('express');
+// const {handleUserSignup}=require('../controllers/user')
+
+// const router=express.Router();
+
+// // signup
+
+// router.post('/',handleUserSignup);
+
+
+
+// module.exports=router;
+
